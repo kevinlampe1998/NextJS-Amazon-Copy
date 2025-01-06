661c7d891939af5389af91bb44adedee94823e13
kevinlampe1998 <kevin.lampe@dci-student.org>
2025-01-06 03:05:15 +0100

    added if expression in middleware to skip public directory

--- src/middleware.js
+++ src/middleware.js
@@ -7,24 +7,32 @@ export function middleware(req) {
     const cookie = req.cookies.get('user');
     console.log('cookie', cookie);
 
-    // if (
+    if (
+        pathname.startsWith('/_next/') ||
+        pathname.startsWith('/public/') ||
+        pathname === '/favicon.ico'
+    ) {
+        return NextResponse.next();
+    }
 
-    //         !cookie && pathname !== '/' &&
-    //         !pathname.startsWith('/frontend/sellers/register') &&
-    //         !pathname.startsWith('/frontend/users/register') &&
-    //         !pathname.startsWith('/frontend/users/sign-in')
+    if (
 
-    //     ) {
-    //     const url = req.nextUrl.clone();
-    //     url.pathname = '/frontend/users/sign-in';
-    //     return NextResponse.redirect(url);
-    // }
+            !cookie && pathname !== '/' &&
+            !pathname.startsWith('/frontend/sellers/register') &&
+            !pathname.startsWith('/frontend/users/register') &&
+            !pathname.startsWith('/frontend/users/sign-in')
+
+        ) {
+        const url = req.nextUrl.clone();
+        url.pathname = '/frontend/users/sign-in';
+        return NextResponse.redirect(url);
+    }
 
     return NextResponse.next();
 }
 
 export const config = {
-    // matcher: [
-    //     '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
-    // ],
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
+    ],
 };
