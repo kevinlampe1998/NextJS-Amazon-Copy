ae899591eeae333442acc8e050e8d8e2815d9d02
kevinlampe1998 <kevin.lampe@dci-student.org>
2025-01-11 23:19:35 +0100

    changed imported res to directly NextResponse.json()

--- src/app/api/sellers/products/read-all/route.js
+++ src/app/api/sellers/products/read-all/route.js
@@ -2,6 +2,7 @@ import res from "@/lib/res";
 import Product from "@/models/Product";
 import Image from "@/models/Image";
 import User from "@/models/User";
+import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
     try {
@@ -14,7 +15,7 @@ export const POST = async (req) => {
 
         if (!sellerId) {
             console.log('req has undefined sellerId');
-            return res({ message: 'req has undefined sellerId', error: 1 });
+            return NextResponse.json({ message: 'req has undefined sellerId', error: 1 });
         }
 
         const products = await Product.find({ seller: sellerId })
@@ -23,16 +24,16 @@ export const POST = async (req) => {
 
         if (!products[0]) {
             console.log('No products found!');
-            return res({ message: 'No products found!', error: 1 });
+            return NextResponse.json({ message: 'No products found!', error: 1 });
         }
 
-        return res({ message: 'Here are your products!', success: 1, products });
+        return NextResponse.json({ message: 'Here are your products!', success: 1, products });
 
     } catch(err) {
 
         console.log('Error on POST Route /api/seller/products/read-all', err);
         console.log('Error on POST Route /api/seller/products/read-all');
-        return res({ message: 'Error on POST Route /api/seller/products/read-all!', error: 1 });
+        return NextResponse.json({ message: 'Error on POST Route /api/seller/products/read-all!', error: 1 });
 
     }
 };
